babbbf9480db8cbbcb067c2829b5b4c162835dc8
felipeflourwears <A01274880@itesm.mx>
2022-12-12 22:48:07 -0600

    Delete CRD and Prueba de React

--- phpImage/database.sql
+++ phpImage/database.sql
@@ -7,3 +7,4 @@ CREATE TABLE imagen(
 	ruta varchar(45),
 	PRIMARY KEY (id)
 );
+
--- phpImage/php/conexion.php
+++ phpImage/php/conexion.php
@@ -1,8 +1,8 @@
 <?php
-$server = "localhost";
-$user = "root";
-$pass = "";
-$db ="c1";
+$server = "IP";
+$user = "";
+$pass = "PASS";
+$db ="DB";
 
 $conexion = mysqli_connect($server, $user, $pass, $db);
 #$conexion = mysqli_connect("localhost", "root", "12345678", "login");
